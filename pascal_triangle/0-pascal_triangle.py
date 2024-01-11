f991454dc33cd0fb27b5577118bc4703435232c4
Edwin-0396 <4639@holbertonschool.com>
2024-01-11 18:16:16 -0500

    Pascal_Triangle task

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-""" Module for Pascal's triangle """
+"""
+    Returns a list of lists of integers representing
+    the Pascal's triangle of n
+"""
 
 
 def pascal_triangle(n):
-    """ Returns list of lists of ints representing Pascal’s triangle of n """
-
+    """Returns a list of lists of integers representing
+    the Pascal's triangle of n"""
     if n <= 0:
-        return []
+        return ([])
 
-    pascal = [[1]]
-    if n == 1:
-        return pascal
-
-    for i in range(1, n):
-        left = -1
-        right = 0
-        in_pas = []
-        for j in range(i + 1):
-            num = 0
-            if left > -1:
-                num += pascal[i - 1][left]
-            if right < i:
-                num += pascal[i - 1][right]
-            left += 1
-            right += 1
-            in_pas.append(num)
-        pascal.append(in_pas)
-    return pascal
+    t_pascal = []
+    i = 1
+    while i < n + 1:
+        j = i
+        k = 0
+        level_p = [1]
+        while j > 2:
+            level_p.append(t_pascal[-1][k] + t_pascal[-1][k + 1])
+            j -= 1
+            k += 1
+        if i > 1:
+            level_p.append(1)
+        t_pascal.append(level_p)
+        i += 1
+    return (t_pascal)
