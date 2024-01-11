d767ed1b765a133417ee6e10651f779c756700fa
Edwin-0396 <4639@holbertonschool.com>
2024-01-11 17:49:52 -0500

    Pascal_Triangle task

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """ Module for calculating Pascal Triangle """
 
+
 def pascal_triangle(n):
-	"""
-	Function for creating a pascal triangle as a list of lists
-    n: number of rows
-    returns empty list if n <= 0
     """
+    Function for creating a pascal triangle as a list of lists
+        n: number of rows
+        returns empty list if n <= 0
+        """
 
-	triangulo = []
+    triangulo = []
 
-	if n <= 0:
-		return triangulo
+    if n <= 0:
+        return triangulo
 
-	for i in range(n):
-		fila = []
-		for j in range(i + 1):
-			if j == 0 or j == i:
-				fila.append(1)
-			else:
-				# Usamos la fórmula del coeficiente binomial
-				coeficiente_binomial = triangulo[i - 1][j - 1] + triangulo[i - 1][j]
-				fila.append(coeficiente_binomial)
-		triangulo.append(fila)
+    for i in range(n):
+        fila = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                fila.append(1)
+            else:
+                coeficiente_binomial = triangulo[i - 1][j - 1] + \
+                    triangulo[i - 1][j]
+                fila.append(coeficiente_binomial)
+                triangulo.append(fila)
 
-	return triangulo
+    return triangulo
