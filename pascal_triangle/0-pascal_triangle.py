45c69a75c45f006d33cb5d4cc5026dc1aa8bfa09
Edwin-0396 <4639@holbertonschool.com>
2024-01-11 18:18:22 -0500

    Pascal_Triangle task

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    Returns a list of lists of integers representing
-    the Pascal's triangle of n
+Returns a list of lists of integers representing
+the Pascal's triangle of n
 """
 
 
