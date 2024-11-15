baf98be1193cbce05e6ff122020f54146c98d7f3
Developer.CQ <developer.cq.colombia@gmail.com>
2024-11-15 14:12:16 -0500

    Valid UTF-8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-""" Function that determines if a given data set represents a valid UTF-8 encoding
+""" 
+Prototype: def validUTF8(data)
+Return: True if data is a valid UTF-8 encoding, else return False
+A character in UTF-8 can be 1 to 4 bytes long
+The data set can contain multiple characters
+The data will be represented by a list of integers
+Each integer represents 1 byte of data, therefore you only need to handle the 8 least significant bits of each integer
 """
 
 def validUTF8(data):
-    """ Function that determines if a given data set represents a valid UTF-8 encoding
+    """ Function that determines if a given data set 
+    represents a valid UTF-8 encoding
     """
-    for data in data:
-        if data > 255:
+    for i in range(len(data)):
+        if data[i] > 255:
             return False
     return True
