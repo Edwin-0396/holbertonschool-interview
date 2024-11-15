3841af883ca617f0bb7496d32819230f0a007cb8
Developer.CQ <developer.cq.colombia@gmail.com>
2024-11-15 14:09:54 -0500

    Valid UTF-8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -2,7 +2,8 @@
 
 
 def validUTF8(data):
-    
+    """ Function that determines if a given data set represents a valid UTF-8 encoding
+    """
     for data in data:
         if data > 255:
             return False
